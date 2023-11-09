bda90fe0cf4abac00db90c6bdbad6be8b013c86a
Mohammed Radwan <gh93st@live.com>
2023-11-09 14:01:36 +0200

    added expenses functionality

--- lib/helpers/db_helper.dart
+++ lib/helpers/db_helper.dart
@@ -3,6 +3,7 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_provider/models/category.dart';
+import 'package:sqflite_provider/models/expense.dart';
 
 class DBHelper {
   String db_name = 'mopense.db';
@@ -44,10 +45,42 @@ class DBHelper {
     dbConn.delete('categories', where: 'catID = ?', whereArgs: [cat.catID]);
   }
 
+  Future<Category> getCategoryByID(int id) async {
+    final dbConn = await getDBConn();
+    List<Map<String, dynamic>> maps =
+        await dbConn.query('categories', where: 'catID = ?', whereArgs: [id]);
+    return Category.fromJson(maps[0]);
+  }
+
   Future<List<Category>> getAllCategories() async {
     final dbConn = await getDBConn();
-    List<Map<String, dynamic>> maps = await dbConn.query('categories');
+    List<Map<String, dynamic>> maps = await dbConn.query(
+      'categories',
+    );
 
     return List.generate(maps.length, (i) => Category.fromJson(maps[i]));
+  }
+
+  Future<int> addExpense(Expense exp) async {
+    final dbConn = await getDBConn();
+    return dbConn.insert('expenses', exp.toJson());
+  }
+
+  Future<int> updateExpense(Expense exp) async {
+    final dbConn = await getDBConn();
+    return dbConn
+        .update('expenses', exp.toJson(), where: 'id = ?', whereArgs: [exp.id]);
+  }
+
+  Future<void> deleteExpense(Expense exp) async {
+    final dbConn = await getDBConn();
+    dbConn.delete('categories', where: 'id = ?', whereArgs: [exp.id]);
+  }
+
+  Future<List<Expense>> getAllExpenses() async {
+    final dbConn = await getDBConn();
+    List<Map<String, dynamic>> maps = await dbConn.query('table');
+
+    return List.generate(maps.length, (i) => Expense.fromJson(maps[i]));
   }
 }
--- lib/models/expense_model.dart
+++ lib/models/expense_model.dart
@@ -3,28 +3,63 @@
 import 'package:flutter/foundation.dart' as fnd;
 import 'package:sqflite_provider/helpers/db_helper.dart';
 import 'package:sqflite_provider/models/category.dart';
+import 'package:sqflite_provider/models/expense.dart';
 
 class ExpenseModel extends fnd.ChangeNotifier {
   List<Category> _categories = [];
   List<Category> get Categories => _categories;
+
+  List<Expense> _expenses = [];
+  List<Expense> get Expenses => _expenses;
+
+  //double _expensesTotal = 0.0;
+  double get ExppensesTotal => getTotalExpenses();
+
+  double getTotalExpenses() {
+    double initial = 0;
+    for (int i = 0; i <= _expenses.length; i++) {
+      initial += _expenses[i].expenseValue!;
+    }
+    return initial;
+  }
 
   void getAllCategories() async {
     _categories = await DBHelper().getAllCategories();
     notifyListeners();
   }
 
-  void addCategory(data) async {
+  void addCategory(Category data) async {
     await DBHelper().addCategory(data);
     notifyListeners();
   }
 
-  void deleteCategory(data) async {
+  void deleteCategory(Category data) async {
     await DBHelper().deleteCategory(data);
     notifyListeners();
   }
 
-  void updateCategory(data) async {
+  void updateCategory(Category data) async {
     await DBHelper().updateCategory(data);
+    notifyListeners();
+  }
+
+  void getAllExpenss() async {
+    _expenses = await DBHelper().getAllExpenses();
+    notifyListeners();
+  }
+
+  void addExpsne(Expense data) async {
+    _expenses = await DBHelper().getAllExpenses();
+    notifyListeners();
+  }
+
+  void updateExpsne(Expense data) async {
+    await DBHelper().updateExpense(data);
+    notifyListeners();
+  }
+
+  void deleteExpsne(Expense data) async {
+    await DBHelper().deleteExpense(data);
     notifyListeners();
   }
 }
